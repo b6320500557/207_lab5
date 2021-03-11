2790ee609d72a38c8879e99dd0ca648045343704
์Natnicha Phriksen <natnicha.phr@ku.th>
2021-03-11 17:27:09 +0700

    add 2

--- 6320500557-5-3.c
+++ 6320500557-5-3.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 int main()
 {
-    int n,j,k;
+    int n,i;
     scanf("%d",&n);
-    /*for(i=1;i<=n;i++)
+   /* for(i=1;i<=4000;i++)
     {
-        if(i%)
-    }*/
+        if(i%2==0 || i%3==0 || i%5==0)
+        {
+            if(i==1)
+                j++;
+            else
+                j++;
+        }
+        if(i == n)
+            break;
+    }
+    printf ("%d",i);*/
     return 0;
 }
